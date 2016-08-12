505e7474d4078a168f40ee2922b5be2a988f474d
Jackson O'Donnell <jasksonhodonnell@gmail.com>
2016-08-11 23:53:54 -0500

    make cond work with quasiquote

--- lisp-src/std-macro.lisp
+++ lisp-src/std-macro.lisp
@@ -1,15 +1,9 @@
-;; really need quasiquote working...
 (define-macro (cond clause . rest)
   (let ((condition (car clause))
         (action (car (cdr clause)))
         (else (if rest 
-                (list (cons (quote cond) rest))
-                (list ()))))
-    ;; (quasiquote 
-    ;;   (if (unquote condition)
-    ;;     (unquote action)
-    ;;     (unquote else)))
-    (cons (quote if)
-          (cons condition
-                (cons action
-                      else)))))
+                (cons 'cond rest)
+                ())))
+    `(if ,condition
+       ,action
+       ,else)))
