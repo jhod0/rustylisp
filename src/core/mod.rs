7199ef0d7daf25b05af47c1f3e6217667688402a
Jackson O'Donnell <jasksonhodonnell@gmail.com>
2016-08-15 16:18:34 -0500

    some macro doc comments and tests

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -10,7 +10,7 @@ macro_rules! symbol {
 }
 
 #[macro_export]
-/// Creates Nil, the empty list
+/// Creates nil, the empty list
 macro_rules! nil {
     () => ( $crate::core::LispObj::LNil )
 }
@@ -34,8 +34,23 @@ macro_rules! float {
 }
 
 #[macro_export]
+/// Quotes a lisp value.
+///
+/// # Example
+/// ```
+/// # #[macro_use] extern crate rustylisp;
+/// # fn main() {
+/// let objects = [symbol!("a"), symbol!("b"),
+///                lisp_list![int!(1), int!(2), int!(3)],
+///                lisp_list![symbol!("a"), symbol!("b"), symbol!("c")]];
+///
+/// for obj in objects.into_iter() {
+///     assert_eq!(quote!(obj), lisp_list![symbol!("quote"), obj]);
+/// }
+/// # }
+/// ```
 macro_rules! quote {
-    ($val:expr) => ( cons!(symbol!("quote"), cons!($val, nil!())) )
+    ($val:expr) => ( lisp_list![symbol!("quote"), $val] )
 }
 
 #[macro_export]
@@ -47,7 +62,7 @@ macro_rules! quote {
 /// # fn main() {
 /// use rustylisp::core::LispObj;
 ///
-/// let raw = LispObj::LString("Hello, World!".to_string());
+/// let raw = LispObj::make_string("Hello, World!");
 /// // Isn't this much nicer?
 /// let with_macro = string!("Hello, World!");
 ///
@@ -59,6 +74,19 @@ macro_rules! string {
 }
 
 #[macro_export]
+/// Creates a Lisp linked list out of its arguments, in the spirit of vec![]
+///
+/// # Example
+/// ```
+/// # #[macro_use] extern crate rustylisp;
+/// # use rustylisp::core::AsLispObjRef;
+/// # fn main() {
+/// let a_list = lisp_list![int!(0), int!(1), int!(2)];
+/// let a_vec  = vec![int!(0).to_obj_ref(), int!(1).to_obj_ref(), int!(2).to_obj_ref()];
+///
+/// assert_eq!(a_list.list_to_vec(), Some(a_vec));
+/// # }
+/// ```
 macro_rules! lisp_list {
     [ $obj:expr ] => {
         cons!($obj, nil!())
@@ -68,19 +96,25 @@ macro_rules! lisp_list {
     }
 }
 
+/// Creates the canonical true value, the symbol 'true
 #[macro_export]
 macro_rules! lisp_true {
     () => ( symbol!("true") )
 }
 
+/// Creates the canonical false value, the symbol 'false
 #[macro_export]
 macro_rules! lisp_false {
     () => ( symbol!("false") )
 }
 
+/// Converts a rust bool into a lisp boolean.
+///
+/// A true value is converted to `lisp_true!()`, a false value 
+/// becomes `lisp_false!()`.
 #[macro_export]
 macro_rules! lisp_bool {
-    ( $val:expr) => {
+    ( $val:expr ) => {
         if $val {
             lisp_true!()
         } else {
